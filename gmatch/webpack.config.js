602f869c1cca303b48e881ee3029e2bf565d0159
Lin_CH <lin_ch@gel-lib.com>
2024-09-26 17:57:11 +0900

    尝试修正cors问题

--- gmatch/webpack.config.js
+++ gmatch/webpack.config.js
@@ -1,6 +1,20 @@
 const { createWebpackConfigAsync } = require('expo-yarn-workspaces/webpack');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = async function(env, argv) {
   const config = await createWebpackConfigAsync(env, argv);
+
+  // 添加代理设置
+  config.devServer = {
+    ...config.devServer,
+    proxy: {
+      '/api': {
+        target: 'https://gmreguserfa.azurewebsites.net', // 后端 API 的基础 URL
+        changeOrigin: true, // 更改请求头中的 origin
+        pathRewrite: { '^/api': '' }, // 将/api前缀重写为根
+      },
+    },
+  };
+
   return config;
 };
